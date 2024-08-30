4e7406c05184c00e67365bf91c40ac10f5ce908f
Juan Carlos <jcruiz1245@gmail.com>
2024-08-29 19:44:16 -0600

    Enlazando htmls

--- services/urls.py
+++ services/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('servicios',views.servicios,name='servicios'),
+	path('servicios/',views.servicios,name='servicios'),
 	path('servicios/quejas_sugerencias',views.quejas_sugerencias,name='quejas_sugerencias'),
 	path('servicios/reporte_alcantarillado',views.reporte_alcantarillado,name='reporte_alcantarillado'),
 	path('servicios/reporte_fugas',views.reporte_fugas,name='reporte_fugas'),
